36d10d9a14d6dd56a29082f8692d87ef0fe3b023
Erick Massa Sprengel <erick.sprengel@gmail.com>
2017-12-31 17:05:09 -0200

    updating article details UI

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/XYZReaderApp.java
@@ -0,0 +1,16 @@
+package com.example.xyzreader.ui;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by erick on 31/12/17.
+ */
+
+public class XYZReaderApp extends Application {
+    public void onCreate() {
+        super.onCreate();
+        //Stetho.initializeWithDefaults(this);
+    }
+}
